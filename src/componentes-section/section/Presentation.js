c661ead53c448e60665d387c676d961022b6ffb0
Rodrigo_MacNiven <rodrigomacniven88@gmail.com>
2023-10-10 21:42:05 -0300

    versao-2
    Melhorias em alguns componentes.
    Coloquei uma interatividade na apresentação (Componente Presentation), uma animação no hover dos dos cards de projetos(Componente Cards) e no hover das habilidades (Componente Skills).

--- src/componentes-section/section/Presentation.js
+++ src/componentes-section/section/Presentation.js
@@ -1,11 +1,43 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
+import { useEffect, useState } from 'react'
 
 function Presentantion(){
+    const [text, setText] = useState('')
+    const toRotate = ['Rodrigo Machado!', 'Desenvolvedor Front End!','Desenvolvedor React!']
+    const [loop, setLoop] = useState(0)
+    const [isDeleting, setIsDeleting] = useState(false)
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return() =>{clearInterval(ticker)}
+    }, [text])
+
+    const toType = () =>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ?  fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+                setIsDeleting(true);
+                setDelta(period);
+            }else if (isDeleting && updatedText === ''){
+                setIsDeleting(false);
+                setDelta(period);
+                setLoop(loop+1);
+            }
+        }
+
     return(
         <div className={styles.presentation} id='Presentation'>
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-            <h1>Olá, eu sou o Rodrigo Machado!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p> Sou um apaixonado por tecnologia e soluções inovadoras. <br />
                 Como Product Manager, eu tenho o compromisso de resolver problemas<br />
                 complexos e trazer resultados excepcionais para os negócios.<br />
